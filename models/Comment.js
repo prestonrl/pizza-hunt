d2843c17290434f561ead9d2002f607a630dad7b
tubasthebest <tubasthebest@gmail.com>
2021-03-20 14:28:37 -0600

    validate data

--- models/Comment.js
+++ models/Comment.js
@@ -8,7 +8,9 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
             type: String
@@ -29,10 +31,13 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
     {
         writtenBy: {
-            type: String
+            type: String,
+            required: true
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdAt: {
             type: Date,
